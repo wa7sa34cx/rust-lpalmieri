00148a40bc62f254184af3f12302c584f1b9f2d9
Alex Black <wa7@sa34.cx>
2021-11-14 21:14:06 +0300

    Chapter 3 done

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+//! main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,58 @@
 //! tests/health_check.rs
 
-use zero2prod::main;
+use std::net::TcpListener;
 
-#[test]
-fn dummy_test() {
-    main()
+// `actix_rt::test` is the testing equivalent of `actix_web::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// Use `cargo add actix-rt --dev --vers 2` to add `actix-rt`
+// under `[dev-dependencies]` in Cargo.toml
+//
+// You can inspect what code gets generated using 
+// `cargo expand --test health_check` (<- name of the test file)
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+
+    // We need to bring in `reqwest` 
+    // to perform HTTP requests against our application.
+    //
+    // Use `cargo add reqwest --dev --vers 0.11` to add
+    // it under `[dev-dependencies]` in Cargo.toml
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    //
+    // New dev dependency - let's add tokio to the party with
+    // `cargo add tokio --dev --vers 1`
+    let _ = tokio::spawn(server);
+
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
